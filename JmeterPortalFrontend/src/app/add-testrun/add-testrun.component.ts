07d723c038f70188e0e5f5c9b461ec3b12527421
Praveen <sankar_praveen@hotmail.com>
2021-08-29 11:52:23 +0530

    Add form control for add-testrun component

--- JmeterPortalFrontend/src/app/add-testrun/add-testrun.component.ts
+++ JmeterPortalFrontend/src/app/add-testrun/add-testrun.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Observable, Subscription, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { FileService } from '../file.service';
 
 @Component({
@@ -15,12 +18,19 @@ export class AddTestrunComponent implements OnInit {
   fileUploadDate: Date = new Date();
   fileBase64: string = "";
   file: any;
+  fileAdded = false;
+  showMsg =  false;
+  successStatus = true;
+  uploadSub! : Subscription;
+  @ViewChild('fileInput' , {static :false}) fileInput! :  ElementRef;
+
   constructor(private uploadService: FileService) { }
 
   ngOnInit(): void {
   }
 
   OnUploadFile(event: Event) {
+    this.fileAdded = true;
     if((event.target as HTMLInputElement).files){
       let files = (event.target as HTMLInputElement).files;
       if(files){
@@ -28,11 +38,17 @@ export class AddTestrunComponent implements OnInit {
       }
     }  
   }
+  Reset(form : NgForm){
+    this.Clear();
+    form.resetForm();
+  }
 
-  Upload(){
-    this.testName = "Sample Test";
-    this.testRunID = "Test Run Id-1"
-    this.environment = "Runway";
+  Clear(){
+    this.fileAdded = false;
+    this.fileInput.nativeElement.value = "";
+  }
+
+  Upload(form : NgForm){
     this.fileName = this.file.name;
     this.fileUploadDate = new Date();
     let base64String = "";
@@ -40,18 +56,21 @@ export class AddTestrunComponent implements OnInit {
     
     reader.onload = ()=>{
       base64String = reader.result!.toString();
-      this.uploadService.UploadFile( {
+      this.uploadSub = this.uploadService.UploadFile( {
         testName : this.testName,
         testRunID : this.testRunID,
         environment : this.environment,
         fileName : this.fileName,
         fileUploadDate : this.fileUploadDate,
         fileStreamDataBase64 : base64String
-      })
-      .subscribe(res=> console.log(res));
+      }).pipe(catchError(err=> { 
+        this.successStatus = !this.successStatus; return throwError(err)
+      })).subscribe(res=> {console.log(res); });
     }
     reader.readAsDataURL(this.file);
-    
+    this.showMsg = true;
+    this.Reset(form);
+    setTimeout(()=> {this.showMsg = false},1500);  
   }
 
 }
